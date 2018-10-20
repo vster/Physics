b7f0e2550fd1e92e0cdfe09f86787d01358a5672
Vital Teresh <vster2006@gmail.com>
2018-10-20 16:07:18 +0300

    add p2-p3

---
+++ 25_Quantum_mechanics/P2_Uncertainty_principle.m
@@ -0,0 +1,45 @@
+% Uncertainty principle
+
+% |B(p)|^2=(sigx^2/pi)*exp(-(p-p0)^2/(2*(hp/2*sigx)^2)      (25-5)
+% |B(p)|^2=(sigx^2/pi)*exp(-(p-p0)^2/(2*sigp^2))            (25-6)
+% Compare (25-5) and (25-6) then
+% sigp=hp/(2*sigx), sigp*sigx=hp/2              (25-7)
+
+% Uncertainty principle
+% delta_x * delta_p >= hp/2                   (25-8)
+
+% Example 1
+% ksi=int B(k)*exp(i*k*x) dk
+
+clear
+syms A k k0 a x real
+ksi=A*int(exp(1i*k*x),k,k0-a,k0+a)
+ksi=simplify(ksi)
+% -(A*(exp(x*(a + k0)*1i) - exp(-x*(a - k0)*1i))*1i)/x
+% -(A*(exp(1i*x*a)*exp(1i*k0*x)-exp(-1i*x*a)*exp(1i*k0*x))/x
+ksi=2*A*(sin(a*x)/x)*exp(1i*k0*x)
+
+ksi_2=simplify(ksi'*ksi)
+% (4*A^2*sin(a*x)^2)/x^2
+
+% a*delta_x=1.39
+% a=delta_k=delta_p/hp => delta_x*delta_p=1.39*hp
+
+% Wave Packet Speed
+% vg=dw/dk
+% hp*w=E, hp*k=p
+% hp*dw/dk=hp^2*k/m
+% dw/dk=hp*k/m=p/m=v => vg=v
+
+% Wave packet spreading
+% delta_x=(delta_vg)*t                  (25-9)
+% deltavg=dvg/dp*delta_p
+% delta_vg=dv/dp*delta_p=1/m*delta_p    (25-10)
+% delta_vg=1/m*(hp/delta_x0)
+% delta_x=hp/(m*delta_x0)*t
+syms hp m delta_x0 t
+delta_x=hp/(m*delta_x0)*t
+delta_x1=subs(delta_x,[hp m delta_x0 t],[1e-34 9e-31 1e-10 1])
+digits(4)
+delta_x1=vpa(delta_x1)
+% 1.111e6 m = 1100 km
---
+++ 25_Quantum_mechanics/P3_Particle_in_the_box.m
@@ -0,0 +1,43 @@
+% Particle in the box
+
+% ksi(x,t)=B*exp(1i*k*x-1i*w*t)-B*exp(-1i*k*x-1i*w*t) =
+%    =B*(exp(1i*k*x)-exp(-1i*k*x))*exp(-1i*w*t)
+
+syms x t k w B syms
+ksi=B*exp(1i*k*x-1i*w*t)-B*exp(-1i*k*x-1i*w*t)
+ksi=B*(exp(1i*k*x)-exp(-1i*k*x))*exp(-1i*w*t)
+
+% sin(k*x)=(exp(1i*k*x)-exp(-1i*k*x))/2i
+sin_kx=(exp(1i*k*x)-exp(-1i*k*x))/2i
+
+y(x,t)=2i*B*exp(-1i*w*t)*sin(k*x)
+% Put A=2*B*1i then
+ksi(x)=A*sin(k*x)                   % (25-11)
+% 0=sin(k*L), if k*L=n*pi
+% k(n)=n*pi/L                       (25-12)
+% L=n*(lam/2)
+% ksi(x,n)=A*sin((n*pi/L)*x)
+% p(n)=hp*k(n)
+% p(n)=n*(pi*hp/L)                  (25-13)
+% E(n)=p(n)^2/(2*m)=n^2*pi^2*hp^2/(2*m*L^2)         (25-14)
+syms n hp m L
+E(n)=n^2*pi^2*hp^2/(2*m*L^2)
+digits(3)
+E1=subs(E,[hp m L],[1.05e-34 9.11e-31 1e-10])
+E1=vpa(E1)
+% 5.97e-18*n^2 J = (37.3*n^2)eV
+
+% Example 2
+% L=10e-10 m, n=2
+% hf=E2-E1
+syms n
+E1=5.97e-18 % J
+E(n)=E1*n^2
+hf=vpa(E(2)-E(1)) 
+% 1.79e-17 J
+h=6.6e-34
+f=hf/h
+% 2.71e16 1/s
+c=3e8 % m/s
+lam=c/f
+% 1.11e-8 m = 111 A
