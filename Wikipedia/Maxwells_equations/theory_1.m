54b557d1c99fe972c8a8e9f3bdf2b614b1b413cd
Vital Teresh <vster2006@gmail.com>
2018-10-29 17:28:57 +0300

    Maxwells_equations

---
+++ Wikipedia/Maxwells_equations/theory_1.m
@@ -0,0 +1,21 @@
+% Formulation in SI units convention
+
+% Integral equations 
+% Gauss's law 
+% int int (d_Omega) E*dS = 1/eps0 int int int (Omega) ro*dV
+% Gauss's law for magnetism
+% int int (d_Omega) B*dS = 0
+% Maxwell Faraday equation (Faraday's law of induction) 
+% int (dSigma) E*dl = -d/dt int int (Sigma) B*dS
+% Ampere's circuital law (with Maxwell's addition) 
+% int(dSigma)B*dl=mu0*(int int(Sigma)J*dS + eps0 d/dt int int(Sigma)E*dS)
+
+% Differential equations 
+% Gauss's law 
+% Nabla*E = ro/eps0
+% Gauss's law for magnetism
+% Nabla*B=0
+% Maxwell Faraday equation (Faraday's law of induction) 
+% Nabla x E = -dB/dt
+% Ampere's circuital law (with Maxwell's addition) 
+% Nabla x B = mu0*(J+eps0*dE/dt)
