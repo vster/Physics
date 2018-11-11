b14619fc8b239f1cfeae4c9a69cfbe33e20cf700
Vital Teresh <vster2006@gmail.com>
2018-11-11 21:57:18 +0300

    add p2_Linear_charge_distribution

---
+++ Physics_Orir/16_Electrostatics/p2_Linear_charge_distribution.m
@@ -0,0 +1,17 @@
+% Linear charge distribution
+
+% As a Gaussian surface we choose a cylinder of length L
+% int()E*dA=4*pi*k0*(lam*L)
+% int()E*dA=E*(2*pi*r*L)
+% 2*pi*r*L*E=4*pi*k0*lam*L
+% Linear charge distribution
+% E=2*k0*lam/r
+
+% The field inside a uniformly charged rod
+% Qin=ro*pi*r^2*L
+% int()E*dA=4*pi*k0*(ro*pi*r^2*L)
+% E*(2*pi*r*L)=4*pi*k0*(ro*pi*r^2*L)
+% Inside the rod
+% E=2*k0*ro*pi*r                   (16-6)
+% lam=ro*pi*R^2
+% E=2*k0*lam*r/R^2                 (16-6a)
---
+++ Physics_Orir/16_Electrostatics/sample03.m
@@ -0,0 +1,4 @@
+% Coaxial cable
+
+% Field created by internal conductor
+% Ein=2*k0*lam/r                    (16-7)
