6c57d9be159bca5b4e58e1073807ba2b5c316cb7
Vital Teresh <vster2006@gmail.com>
2018-11-12 13:58:26 +0300

    modify task 1.8

--- Quantum_Mechanics_Parfenov/1_Basic_concepts/task_1_8.m
+++ Quantum_Mechanics_Parfenov/1_Basic_concepts/task_1_8.m
@@ -8,3 +8,12 @@
 % Psi(phi)=A*exp(i*n*phi)
 % int (0:2*pi) |Psi|^2 d_phi = A^2*2*pi => A=1/sqrt(2*pi)
 % Psi(phi,n) = 1/sqrt(2*pi) exp(i*n*phi)
+
+clear
+syms phi A n real
+Psi=A*exp(i*n*phi)
+I=int(abs(Psi)^2,phi,0,2*pi)
+% 2*pi*A^2=1 => 
+A1=1/sqrt(2*sym(pi))
+Psi=subs(Psi,A,A1)
+% (2^(1/2)*exp(n*phi*1i))/(2*pi^(1/2))
