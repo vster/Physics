bdd7cbdd103e62d572ad7039fb67809637850414
Vital Teresh <vster2006@gmail.com>
2018-11-13 17:50:32 +0300

    add ex 1.9-1.10

---
+++ Quantum_Mechanics_Parfenov/1_Basic_concepts/task_1_10.m
@@ -0,0 +1,25 @@
+clear
+syms k x
+
+% a)
+Psi=exp(1i*k*x)
+pxm=Psi'*OperP(Psi)
+% hp*k
+
+% b)
+syms sigx
+Psi=cos(k*x)
+pxm=int(Psi'*OperP(Psi),x,-sigx,sigx)
+% 0
+
+% c)
+syms a x sigx
+Psi=exp(-a*x^2)
+pxm=int(Psi'*OperP(Psi),x,-sigx,sigx)
+% 0
+
+% Impulse operator
+function P=OperP(psi)
+syms x hp
+P=-1i*hp*diff(psi,x);
+end
--- Quantum_Mechanics_Parfenov/1_Basic_concepts/task_1_9.m
+++ Quantum_Mechanics_Parfenov/1_Basic_concepts/task_1_9.m
@@ -1,9 +1,16 @@
-syms hi k x real
-Psi=cos(hi)*exp(i*k*x)+sin(hi)*exp(-i*k*x)
-px=OperP(Psi)
+syms hi k x 
+Psi=cos(hi)*exp(1i*k*x)+sin(hi)*exp(-1i*k*x)
 
-nf=int(simplify(Psi'*Psi),x,-inf,inf)
+px=simplify(Psi'*OperP(Psi))
+% hp*k*(cos(2*hi) + sin(2*hi)*sin(2*k*x)*1i)
+pxm=int(simplify(Psi'*OperP(Psi)),x)
 
-pxm=int(Psi'*k*Psi,x,-inf,inf)
+nf=int(Psi'*Psi,x,-pi/k,pi/k)
 
 % ...?
+
+% Impulse operator
+function P=OperP(psi)
+syms x hp 
+P=-1i*hp*diff(psi,x);
+end
