f8f8d5aa20d9ef1b765455a01d7ef54e63ab344f
Vital Teresh <vster2006@gmail.com>
2018-11-14 20:19:26 +0300

    Start TOE. add task 1.1-1.4

---
+++ TOE/C1_Calculation_of_linear_electrical_circuits/p_1_1_Passive_elements.m
@@ -0,0 +1,29 @@
+% Passive elements
+
+% Ohm's law
+% u=R*i
+% or
+% i=G*u
+
+% Amount of heat energy WT
+% WT=int(0:t) i^2*R dt
+
+% Inductance L
+% WM=L*i^2/2 - energy in the circuit with L
+
+% L=Psi/i
+% Psi - flux linkage
+
+% Psi=Phi*w
+% Phi - magnetic flux
+% w - number of turns
+
+% i=1/L*int u dt
+% u= L di/dt
+
+% Capacity C
+% WE=C*u^2/2 - energy in the circuit with C
+
+% C=q/u
+% i=C*du/dt
+% u=1/C*int i dt
---
+++ TOE/C1_Calculation_of_linear_electrical_circuits/p_1_2_Sources_of_electrical_energy.m
@@ -0,0 +1,10 @@
+% Sources of electrical energy
+
+% E - Voltage source
+% J - Current source
+
+% J=Es/Rs
+% Es=Rs*J
+
+% Generalized Ohm's Law
+% I=(U + sum()E)/(sum()R)
