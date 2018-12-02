90685210464aa9c96c95c3df01c585fe02929f9f
Vital Teresh <vster2006@gmail.com>
2018-12-02 15:27:10 +0300

    add p_3_3_Resonance, task 3.15-3.19

---
+++ TOE/C3_Single_phase_electric_circuits_of_sinusoidal_current/p_3_3_Resonance.m
@@ -0,0 +1,25 @@
+% Resonance in sinusoidal current circuits
+
+% Resonance in a series circuit of elements RLC
+% phi=0 or x=0
+% x=w*L-1/(w*C)=0 => w*L=1/(w*C)
+
+% Resonance frequency
+% w0=1/sqrt(L*C)
+
+% Contour quality factor
+% Q=UC0/U=UL0/U
+
+% Contour attenuation
+% d=1/Q
+
+% Resonance in a parallel circuit of elements
+% phi=0
+% b=bL-bC=0 or 1/(wL)-w*C=0
+
+% Resonance frequency
+% w0=1/sqrt(L*C)
+
+% Contour quality factor
+% Q=IC0/I=IL0/I
+
