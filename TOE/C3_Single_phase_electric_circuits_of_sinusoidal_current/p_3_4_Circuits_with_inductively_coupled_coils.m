ac5e77e8209d8b308fffcce9d035b408bb91ba33
Vital Teresh <vster2006@gmail.com>
2018-12-02 17:02:38 +0300

    p_3_4_Circuits_with_inductively_coupled_coils

---
+++ TOE/C3_Single_phase_electric_circuits_of_sinusoidal_current/p_3_4_Circuits_with_inductively_coupled_coils.m
@@ -0,0 +1,17 @@
+% Circuits with inductively coupled coils
+
+% Self-induction flow coupling
+% PsiL1=w1*Phi                  (3.36)
+% where
+% w1 - the number of turns of the first coil
+% Phi - magnetic flux passing through one coil
+
+% Emf self induction
+% EL=-d_PsiL/dt                 (3.37)
+
+% EMF of mutual induction
+% EM=-d_Psi_M/dt=-M*d_i/dt      (3.38)
+
+% Voltages u1 and u2 of each inductively coupled coil
+% u1=uL1+-uM12=d_PsiL1/dt+-d_PsiM12/dt=L1*d_i1/dt+-M*d_i2/dt      (3.39)
+% u2=uL2+-uM21=d_PsiL2/dt+-d_PsiM21/dt=L2*d_i2/dt+-M*d_i1/dt
