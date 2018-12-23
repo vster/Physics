7d22a1dde79d163ce184b36c25af3196968a00ff
Vital Teresh <vster2006@gmail.com>
2018-12-23 16:21:25 +0300

    p3_DC_circuits

---
+++ Physics_Orir/17_Electric_current_and_magnetic_power/p3_DC_circuits.m
@@ -0,0 +1,24 @@
+% DC circuits
+
+% Rt=V/I
+
+% Serial connection
+% V=V1+V2+V3  | :I
+% Rt=R1+R2+R3
+
+% Parallel connection
+% I=I1+I2+I3   | :V
+% 1/Rt=1/R1+1/R2+1/R3
+
+% Kirchhoff rules
+
+% ABCDEA
+% Emf2-I2*R2-I3*R1=0
+% EFDE
+% Emf1-I3*R1=0 => I3=Emf1/R1
+% Point D
+% I1+I2-I3=0
+
+% Emf2-Emf1-I2*R2=0 => I2=(Emf2-Emf1)/R2
+% I1=I3-I2=Emf1/R1-(Emf2-Emf1)/R2
+% I1=Emf1*(1/R1+1/R2)-Emf2/R2
