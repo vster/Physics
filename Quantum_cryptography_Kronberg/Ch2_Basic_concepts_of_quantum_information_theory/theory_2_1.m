26763481ec1c81868997f5103906f5c0b3d3c85b
Vital Teresh <vster2006@gmail.com>
2019-01-09 00:24:39 +0300

    modify theory 2.1

--- Quantum_cryptography_Kronberg/Ch2_Basic_concepts_of_quantum_information_theory/theory_2_1.m
+++ Quantum_cryptography_Kronberg/Ch2_Basic_concepts_of_quantum_information_theory/theory_2_1.m
@@ -28,4 +28,26 @@ OperRo=[p1 0;0 p2]
 simplify(bra_psi*OperRo*ket_phi)
 % p1*phi1*conj(psi1) + p2*phi2*conj(psi2)
 
+% A=sum(i)lam(i)|lam(i)><lam(i)|
+
+% State change over time
+
+% i*hp*d|psi>/dt=H|psi>                      (2.1)
+% U=exp(i*H)
+% |psi'>=U|psi>                              (2.2)
+
+% Qubits
+
+ket0=[1;0]
+ket1=[0;1]
+syms alfa real
+ket_psi=cos(alfa)*ket0+sin(alfa)*ket1
+bra_psi=ket_psi'
+ket_psi1=cos(alfa+pi/2)*ket0+sin(alfa+pi/2)*ket1   % ?
+bra_psi1=ket_psi1'
+
+syms p
+ro=p*ket_psi*bra_psi+(1-p)*ket_psi1*bra_psi1
+ro=simplify(ro)
+% ???
 
