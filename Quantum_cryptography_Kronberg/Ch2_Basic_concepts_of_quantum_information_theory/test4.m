7fd8a334f2a9c5a6fbb2d031ffc0ce4fb637121a
Vital Teresh <vster2006@gmail.com>
2019-01-13 16:40:38 +0300

    modify test4

--- Quantum_cryptography_Kronberg/Ch2_Basic_concepts_of_quantum_information_theory/test4.m
+++ Quantum_cryptography_Kronberg/Ch2_Basic_concepts_of_quantum_information_theory/test4.m
@@ -6,5 +6,6 @@ bra_phi_i=ket_phi_i'
 OpP=ket_phi_i*bra_phi_i
 OpP'
 
-syms lam1 lam2
-ket_psi=lam1*ket_phi_i(:,1)+lam2*ket_phi_i(:,2)
+% syms lam1 lam2
+lam=sym('lam',[1,2])
+ket_psi=lam(1)*ket_phi_i(:,1)+lam(2)*ket_phi_i(:,2)
