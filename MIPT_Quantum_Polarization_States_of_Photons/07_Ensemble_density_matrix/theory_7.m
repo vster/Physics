58eac7cb86e7a4bd17bcef7f57358bba2b0eb35a
Vital Teresh <vster2006@gmail.com>
2019-01-21 10:15:54 +0300

    add theory 7

---
+++ MIPT_Quantum_Polarization_States_of_Photons/07_Ensemble_density_matrix/theory_7.m
@@ -0,0 +1,16 @@
+% Let's consider ensemble of pure states {p(i),|psi(i)>}
+% sum(i)p(i)=1, then
+% <OpA>=sum(i)p(i)*<psi(i)|OpA|psi(i)=tr(OpA*OpRo)             (28)
+% where
+% OpRo=sum(i)p(i)|psi(i)><psi(i)| - density operator
+% tr(OpM>=sum(i)<i|OpM|i>=sum(i)M(i,i) - operator trace
+
+% Density matrix Ro with elements ro(i,j)=<i|OprRo|j>
+% have next propeties
+% 1) Hermitage Ro'=Ro;
+% 2) not negative Ro>=0;
+% 3) Unity trace tr(Ro)=1
+
+
+
+
