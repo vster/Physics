144b333d3dfa0ff6ad272bc373f52cca487babb9
Vital Teresh <vster2006@gmail.com>
2019-01-21 17:18:49 +0300

    add theory 7-8

--- MIPT_Quantum_Polarization_States_of_Photons/07_Ensemble_density_matrix/theory_7.m
+++ MIPT_Quantum_Polarization_States_of_Photons/07_Ensemble_density_matrix/theory_7.m
@@ -11,6 +11,30 @@
 % 2) not negative Ro>=0;
 % 3) Unity trace tr(Ro)=1
 
+% 1) OpRo'=sum(i)p(i)(|psi(i)><psi(i)|)'=
+%              sum(i)p(i)*|psi(i)><psi(i)|=OpRo               (29)
+% 2) <hi|OpRo|hi>=sum(i)p(i)<hi|psi(i)><psi(i|hi>=
+%              sum(i)p(i)|<hi|psi(i)>|^2>=0                   (30)
+% 3) tr(OpRo>=sum(i)p(i)*tr(|psi(i)><psi(i)|)=
+%              sum(i)p(i)<psi(i)|psi(i)>=sum(i)p(i)=1         (31)
 
+clear
+ket_H=[1;0]
+ket_V=[0;1]
+bra_H=ket_H'
+bra_V=ket_V'
 
+Ro=sym('p',[2 2])
+
+% Ro=(ro*H*H ro*H*V)  = 1/2*(1+s3    s1-i*s2)                 (32)
+%    (ro*V*H Ro*V*V)        (s1+i*s2 1-s3   )
+% where
+% si=<sig(i)>=tr(ro*sig(i)) - Stokes parameters in general
+
+% ro>=0 <=> s1^2+s2^2+s3^2<=1
+
+% OpRo=|psi><psi| - pure states
+% OrPo~=|psi><psi| - mix states
+
+% tr(ro^2)=1 <=> State is pure
 
---
+++ MIPT_Quantum_Polarization_States_of_Photons/08_Partially_Polarized_Light/theory_08.m
@@ -0,0 +1,23 @@
+% Partially Polarized Light
+
+% E=E0(t)*exp(-i*w*t)
+% J=<E*E'>=<E0*E0'> - Average wave intensity
+
+% Polarization tensor of classical electromagnetic radiation
+% ro(alfa,beta)=1/J*<E(alfa)*E(beta)'>=1/J*<E0(alfa)*E0(beta)'>
+% where
+% alfa,beta=1,2 - directions in space orthogonal 
+% to the direction of wave propagation
+% Note, that sum(alfa)ro(alfa,alfa)=1
+
+% The density matrix introduced in quantum theory ro(alfa,beta) 
+% coincides with the polarization tensor ro(alfa,beta)
+
+% The Stokes parameters introduced earlier determine 
+% the degree of polarization.
+% P=sqrt(s1^2+s^2+s3^2)
+
+% For unpolarized light
+% ro=1/2*[1 0;0 1] and P=0
+
+
