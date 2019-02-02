6128d05bad1e37ee9f5342e475adbf5349800f82
Vital Teresh <vster2006@gmail.com>
2019-02-02 15:43:28 +0300

    add theory 5.6

---
+++ Quantum_Physics_Martinson/5_Quantum_theory_of_the_atom/theory_5_6.m
@@ -0,0 +1,48 @@
+% Atom in a magnetic field
+
+% Atomic magnetic moment
+% 1. The orbital and spin moments of each electron are added 
+% at the total moment. Observed in heavy atoms.
+
+% 2. In light and medium atoms there is a LS-connection.
+% A) All orbital mechanical moments
+% LL=hp*sqrt(L*(L+1))                      (5.51)
+
+% B) Spin Moments of All Electrons
+% LS=hp*sqrt(S*(S+1))                      (5.52)
+% Values are from 0 to 1/2*N
+
+% C) Resulting Moment
+% LJ=LL+LS
+% LJ=hp*sqrt(J*(J+1))                      (5.53)
+% where
+% J=L+S, L+S-1, ... |L-S|
+
+% The projection of the resulting mechanical moment 
+% on the selected direction
+% LJz=mJ*hp                                (5.54)
+% here
+% mJ=-J, -J+1, ... (J-1), +J
+
+% Quantum number in a multielectron atom
+% 2S+1
+%     L
+%      J
+% where 
+% L...    0 1 2 3 4 ...
+% Letter  S P D F G ...
+
+% Gyromagnetic ratio for total mechanical and magnetic moments
+% PMJ=g*muB*sqrt(J*(J+1))                 (5.55)
+% where
+% g=1+(J*(J+1)+S*(S+1)-L*(L+1))/(2*J*(J+1))    (5.56)
+% called Lande factor
+
+% The projection of the resulting magnetic moment of the atom
+% PMJz=g*muB*mJ                           (5.57)
+
+% Significantly different from zero probabilities 
+% have only transitions in which
+% D_L=0, +-1
+% D_S=0
+% D_J=0, +-1.
