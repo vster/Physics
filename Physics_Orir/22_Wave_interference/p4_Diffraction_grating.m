a4a0a730ebf2065ba1610f954b6ec657fd893296
Vital Teresh <vster2006@gmail.com>
2019-02-03 12:01:16 +0300

    modify p4_Diffraction_grating, add sample 6

--- Physics_Orir/22_Wave_interference/p4_Diffraction_grating.m
+++ Physics_Orir/22_Wave_interference/p4_Diffraction_grating.m
@@ -1,5 +1,6 @@
 % Diffraction grating
 clear
+syms I0 N phi
 I=I0*sin(N*(phi/2))^2/sin(phi/2)^2
 % where
 % phi=k*d*sin(teta)
@@ -15,3 +16,22 @@ I1=N^2*I0
 
 % d*sin(teta)=n*lam
 % or sin(teta)=n*lam/d
+
+% Resolution
+
+% N*d_phi0/2=pi
+% or
+% d_phi0=2*pi/N
+syms k d teta0
+phi0(teta0)=k*d*sin(teta0)
+d_phi0=diff(phi0,teta0)
+% d*k*cos(teta0)*d_teta0
+syms d_phi0
+d_teta0=d_phi0/(d*k*cos(teta0))
+d_teta0=2*pi/(N*d*k*cos(teta0))
+syms lam0
+d_teta0=lam0/(N*d*cos(teta0))             % (22-8)
+
+% sin(teta)=lam/d => d_lam=d*cos(teta)*d_teta
+% Use (22-8), then
+% d_lam0=lam0/N                             (22-9)
---
+++ Physics_Orir/22_Wave_interference/sample_6.m
@@ -0,0 +1,10 @@
+clear
+lam=5893 
+lam1=5890
+lam2=5896
+
+d_lam=lam2-lam1
+
+lam/d_lam
+%   982
+% 13400 (from sample 5) > 982
