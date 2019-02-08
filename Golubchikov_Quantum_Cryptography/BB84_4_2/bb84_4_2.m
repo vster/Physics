a0fcb9174aa5cf23513446a799d772a9154aef6f
Vital Teresh <vster2006@gmail.com>
2019-02-08 13:00:30 +0300

    modif bb84_4_2

--- Golubchikov_Quantum_Cryptography/BB84_4_2/bb84_4_2.m
+++ Golubchikov_Quantum_Cryptography/BB84_4_2/bb84_4_2.m
@@ -3,7 +3,7 @@ clear
 format short
 digits(2)
 global eta ket0 ket1
-eta=2*pi/3
+eta=pi/3
 ket0=[1;0];
 ket1=[0;1];
 
@@ -62,15 +62,19 @@ disp('Bob Pr Vect')
 disp(PrVectB(1:10))
 
 EqBas=0;
+good=0;
 err=0;
 EqBasVect=zeros(1,size);
 ValidDataVect=zeros(1,size);
 for n=1:size
-    if BasisA(n)==BasisB(n)
+    if round(PrVectB(n),2)==1
+        ValidDataVect(n)=1;
+    end
+    if BasisA(n)==BasisB(n) && ValidDataVect(n)==1
         EqBasVect(n)=1;
         EqBas=EqBas+1;
-        if round(PrVectB(n),2)==1
-            ValidDataVect(n)=1;
+        if DataA(n)==DataB(n)
+            good=good+1;
         else
             err=err+1;
         end
@@ -82,6 +86,7 @@ disp('Valid Data')
 disp(ValidDataVect(1:10))
 ber_eq=err/EqBas
 ber_size=err/size
+good_size=good/size
 
 function Psi=Snd(Data,Basis)
 global eta ket0 ket1
