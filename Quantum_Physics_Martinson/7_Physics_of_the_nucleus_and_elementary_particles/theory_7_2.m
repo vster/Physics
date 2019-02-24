3393f55cd7c0cef5ce338da85ecf36be2aa45038
Vital Teresh <vster2006@gmail.com>
2019-02-24 15:54:18 +0300

    add theory 7.2

---
+++ Quantum_Physics_Martinson/7_Physics_of_the_nucleus_and_elementary_particles/theory_7_2.m
@@ -0,0 +1,13 @@
+% Radioactive nuclear decay
+
+% Mm*c^2=Md*c^2+sum(i)m(i)*c^2+Ek                 (7.12)
+
+% Mm > Md+sum(i)m(i)                              (7.13)
+
+% Law of radioactive decay
+
+% dN=-lam*N*dt                                    (7.14)
+% N(t)=N0*exp(-lam*t)                             (7.15)
+% N0-N(t)=N0*(1-exp(-lam*t)                       (7.16)
+% A=lam*N=-dN/dt=lam*N0*exp(-lam*t)=A0*exp(-lam*t)    (7.17)
+% A0=lam*N0 - The activity of the radioactive drug at time t = 0
