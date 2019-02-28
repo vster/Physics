0e29dbb289c7a496206fc612e768231dd968c4bf
Vital Teresh <vster2006@gmail.com>
2019-02-28 10:23:43 +0300

    modify theory 1.1.1

--- Liokumovich_Fiber_Optic_Interferometric_Measurements/theory_1_1_1.m
+++ Liokumovich_Fiber_Optic_Interferometric_Measurements/theory_1_1_1.m
@@ -19,7 +19,34 @@ E2=Em2*sin(w*t+phi2)                               % (1.3)
 
 % The level of manifestation of interference characterizes 
 % visibility (visibility function, contrast)
-% V=(Imax-imin)/(Imax+imin)                          (1.8)
+% V=(Imax-Imin)/(Imax+Imin)                          (1.8)
 % where
 % Imax=I1+I2+2*sqrt(I1*I2)
 % Imin=I1+I2-2*sqrt(I1*I2)
+syms I1 I2
+Imax=I1+I2+2*sqrt(I1*I2)
+Imin=I1+I2-2*sqrt(I1*I2)
+V=(Imax-Imin)/(Imax+Imin)
+% (4*(I1*I2)^(1/2))/(2*I1 + 2*I2)
+V=simplify(V)
+% (2*(I1*I2)^(1/2))/(I1 + I2)
+V=2*sqrt(I1/I2)/((I1/I2)+1)
+syms V phi1 phi2
+I=(I1+I2)*(1+V*cos(phi1-phi2))                      % (1.9)
+
+syms t Em1(t) Em2(t) phi1(t) phi2(t)
+E1(t)=Em1(t)*sin(w*t+phi1(t))
+E2(t)=Em2(t)*sin(w*t+phi2(t)) 
+E(t)=E1(t)+E2(t)
+syms tau x
+E2(t)=E(t)^2
+E2(t)=expand(E2(t))
+E2m=1/tau*int(E2,x,t-tau,t)
+E2m=simplify(E2m)
+% Em1(t)^2/2 + Em2(t)^2/2 - (cos(2*phi1(t) + 2*t*w)*Em1(t)^2)/2 - (cos(2*phi2(t) + 2*t*w)*Em2(t)^2)/2 - Em1(t)*Em2(t)*cos(phi1(t) + phi2(t) + 2*t*w) + Em1(t)*Em2(t)*cos(phi1(t) - phi2(t))
+% <E^2>=1/tau*int(t-tau:t)E(x)^2*dx
+% <E^2>=<Em1^2>+<Em2^2>+
+%    +2*1/tau*int(t-tau:t)Em1(x)*Em2(x)*cos(phi1(x)-phi2(x))dx   (1.11)
+
+% I(t)=I1(t)+I2(t)+2*sqrt(I1(t)*I2(t))*cos(phi1(t)-phi2(t))      (1.12)
+% This result matches (1.6)
