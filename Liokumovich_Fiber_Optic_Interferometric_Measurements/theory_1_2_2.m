dce0d2a217c1e2982272129d757f96da20d7ed3c
Vital Teresh <vster2006@gmail.com>
2019-02-28 17:15:33 +0300

    add theory 1.2.2

---
+++ Liokumovich_Fiber_Optic_Interferometric_Measurements/theory_1_2_2.m
@@ -0,0 +1,13 @@
+% 1) X-couplers are used to form fiber interferometers.
+
+%  1 \_/ 3
+%  2 / \ 4
+
+% Ijout=kij*Iiin                                   (1.31)
+% sum(j)kij=1
+% kii=0
+% k12=k21=k34=k43=0
+% kij=kji and k13=k24=k31=k42, k14=k23=k41=k32
+% k=k13
+% k31=k24=k42=k
+% k14=k41=k23=k32=1-k                              (1.32)
