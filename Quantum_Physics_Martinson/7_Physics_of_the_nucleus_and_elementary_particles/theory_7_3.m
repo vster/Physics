907f0730862b07c48c4d6608a8686a024642efd1
Vital Teresh <vster2006@gmail.com>
2019-03-03 15:24:32 +0300

    add theory 7.3

---
+++ Quantum_Physics_Martinson/7_Physics_of_the_nucleus_and_elementary_particles/theory_7_3.m
@@ -0,0 +1,37 @@
+% Nuclear reactions
+
+% Basic concepts and notation
+% Nuclear reactions
+% a + X(Z,A) -> b +  Y(Z1,A1)                   (7.27)
+% X(Z,A)(a,b)Y(Z1,A1)                           (7.28)
+
+% alfa + N(7,14) -> p + O(8,17)                 (7.29)
+
+% direct reactions
+% (n,n), (n,p), (p,n), (p,p)
+% breakdown reaction
+% (d,p), (n,d)
+% pickup reactions
+% (p,d), (n,d)
+
+% a + X(Z,A) -> Pi -> b + Y(Z1,A1)             (7.30)
+% alfa + Be(4,8) -> C(6,13) -> n + C(6,12)     (7.31)
+
+% a + X -> b + Y + Q                           (7.32)
+% where Q - energy released during the reaction
+% If Q>0 then the reaction is called exodermic
+% If Q<0 then the reaction is called endodermic
+
+% Effective nuclear reaction cross section
+% dNc=nc*S*dx
+% sig*nc*S*dx/S = sig*nc*dx
+% dP=sig*nc*dx                                 (7.33)
+% J*dP=J*sig*nc*dx
+% dJ=-J*sig*nc*dx
+% J=J0*exp(-sig*nc*x)
+% sig=1/(nc*d)*log(J0/J(d))                    (7.34)
+
+% Unit 1 barn = 1e-28 m^2
+% sig ~ E^(-1/2)
+% For most reactions
+% sig = 0.1...10 barn
