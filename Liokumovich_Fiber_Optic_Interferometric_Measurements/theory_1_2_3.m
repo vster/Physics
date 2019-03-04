217bca90aca13ee5158dcd7af564bcd3c00bf6b6
Vital Teresh <vster2006@gmail.com>
2019-03-04 14:38:25 +0300

    modify theory 1.2.3

--- Liokumovich_Fiber_Optic_Interferometric_Measurements/theory_1_2_3.m
+++ Liokumovich_Fiber_Optic_Interferometric_Measurements/theory_1_2_3.m
@@ -1,1 +1,25 @@
-Fiber interferometric circuits (analogs of classical circuits)
+% Fiber interferometric circuits (analogs of classical circuits)
+
+% -> --<1>-----<2>---- ->
+%        \-----/
+% Scheme Maha-Zehnder
+
+% ->  -----\  /------|
+%           <>
+% <-  -----/  \------|
+% Michelson's scheme
+
+% -> --|----/\-----|-- ->
+%           \/ 
+% Scheme Fabri-Pero
+
+% ->  ---\  /--- ->
+%         <>
+%        /  \
+%        \--/
+% Ring circuit
+
+% ->  ----\  /-----\
+%          <>     | | 
+% <-  ----/  \-----/
+% Sagnac scheme
