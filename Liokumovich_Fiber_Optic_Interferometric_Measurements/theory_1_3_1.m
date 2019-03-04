2e8f82e942ac82913492e43c6193fe7abfb46159
Vital Teresh <vster2006@gmail.com>
2019-03-04 15:38:27 +0300

    add theory 1.3.1, 1.3.2.1

---
+++ Liokumovich_Fiber_Optic_Interferometric_Measurements/theory_1_3_1.m
@@ -0,0 +1,15 @@
+% Fiber Interferometer Signal
+
+% Iout=Pout/S                           (1.36)
+% where
+% S - effective fiber cross-sectional area
+
+% Iout -> I(t)
+
+% I(t)=I(D_phi(t))                      (1.37)
+
+% Interference signal contrast
+% V=(Imax-Imin)/(Imax+Imin)             (1.38)
+% Analogous to the visibility of the interference pattern
+
+
---
+++ Liokumovich_Fiber_Optic_Interferometric_Measurements/theory_1_3_2_1.m
@@ -0,0 +1,15 @@
+% Signal of two-beam interferometers
+
+% I(t)=I1+I2+2*sqrt(I1*I2)*cos(D_phi(t))               (1.39)
+
+% D_phi=phi1-phi2=beta1*l1-beta2*l2=
+%              =(2*pi/lam)*(L1-L2)=(2*pi/lam)*D_L      (1.40)
+% l1,l2 - geometric lengths of light guides
+% L1,L2 - optical fiber lengths
+
+% If beta1=beta2=beta=2*pi*n/lam then
+% D_phi=beta*D_l=(2*pi*n/lam)*D_l=(2*pi*n*nu/c)*D_l        (1.41)
+
+% V=2*sqrt(I1*I2)/(I1+I2)=2*sqrt(I1/I2)((I1/I2)+1)         (1.42)
+
+% I(t)=(I1+I2)*(1+V*cos(D_phi(t)))                         (1.43)
