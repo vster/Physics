33f15d6e0987c82f7defc8cb54ec718d0f792d11
Vital Teresh <vster2006@gmail.com>
2019-07-24 21:34:39 +0300

    mod 18_Magnetic_fields

--- Physics_Orir/18_Magnetic_fields/p5_Maxwell_equations.m
+++ Physics_Orir/18_Magnetic_fields/p5_Maxwell_equations.m
@@ -2,12 +2,34 @@
 
 % MKS unit system, using k0
 % I.   int_c()E*dA=4*pi*k0*Q    - Gauss theorem
+syms A E(A) k0 Q
+eq1=int(E(A),A)==4*pi*k0*Q
+
 % II.  int_c()E*ds=0
+syms s E(s)
+eq2=int(E(s),s)==0
+
 % III. int_c()B*dA=0
+syms A B(A)
+eq3=int(B(A),A)==0
+
 % IV.  int_c()B*ds=(4*pi*k0/c^2)*I  - Ampere's Law
+syms s B(s) k0 c I
+eq4=int(B(s),s)==(4*pi*k0/c^2)*I 
 
 % MKS unit system, using eps0 and mu0
-% I.   int_c()E*dA=(1/eps0)*Q    - Gauss theorem
+% I.   int_c()E*dA=(1/epsilon0)*Q    - Gauss theorem
+syms A E(A) epsilon0 Q
+eq1=int(E(A),A)==(1/epsilon0)*Q
+
 % II.  int_c()E*ds=0
+syms s E(s)
+eq2=int(E(s),s)==0
+
 % III. int_c()B*dA=0
+syms A B(A)
+eq3=int(B(A),A)==0
+
 % IV.  int_c()B*ds=mu0*I  - Ampere's Law
+syms s B(s) mu0 I
+eq4=int(B(s),s)==mu0*I
--- Physics_Orir/18_Magnetic_fields/p6_Main_conclusions.m
+++ Physics_Orir/18_Magnetic_fields/p6_Main_conclusions.m
@@ -1,29 +1,44 @@
 % Main conclusions
-
+clear
 % Ampere's Law
-% int_c()B*ds=4*pi*(k0/c^2)*Itotal
+% int_c()B*ds=4*pi*(k0/c^2)*I_total
+syms s B(s) k0 c I_total
+eq1=int(B(s),s)==4*pi*(k0/c^2)*I_total
 
 % Full magnetic flux through a closed surface
 % int_c()B*dA=0
+syms A B(A)
+eq2=int(B(A),A)==0
 
 % Bio-Savara Law
 % dB=(k0/c^2)*I*cross(dI,r)/r^2  
+syms I dI R r
+dB=(k0/c^2)*I*dI*R/r^2
 
-% The field inside the solenoid with nl turns per unit length
-% B=4*pi*(k0/c^2)*I*nl
+% The field inside the solenoid with n_l turns per unit length
+% B=4*pi*(k0/c^2)*I*n_l
+syms I n_l
+B=4*pi*(k0/c^2)*I*n_l
 
 % The magnetic field created by the rectilinear current
 % B=(2*k0/c^2)*(I/r)
+B=(2*k0/c^2)*(I/r)
 
 % The magnetic field of the flat current
 % B=2*pi*(k0/c^2)*I
+B=2*pi*(k0/c^2)*I
 
 % Effective magnetic charge (if any exist) 
 % is expressed through surface current.
 % qm=I1*A
+syms I1 A
+qm=I1*A
 
 % Magnetic moment loop with current covering area A
 % mu=I*A
+mu=I*A
 
 % Electron magnetic moment
-% mue=(e/(2*m))*h/(2*pi)
+% mu_e=(e/(2*m))*h/(2*pi)
+syms e m h
+mu_e=(e/(2*m))*h/(2*pi)
